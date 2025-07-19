9f2a1aaf65b4087ce523383b1b09ae0661c1069c
chaoky <levimanga@gmail.com>
2025-07-19 20:40:12 -0300

    feat: support zsh and bash

--- bigShell.nix
+++ bigShell.nix
@@ -17,13 +17,37 @@ let
   shellName = if shell == "auto" then userShell else shell;
   wrapped = pkgs.writeShellScriptBin shellName (
     {
-      "fish" = "exec fish --init-command='${shellHook}'";
-      "bash" = "...";
-      "zsh" = ''
+      fishHook = "exec fish --init-command='${shellHook}'";
+      bashHook = ''
+        INIT=/tmp/bigShellNix
+        mkdir -p $INIT
+
+        printf "%s\n" \
+          "source $HOME/.bashrc" \
+          '${shellHook}' \
+        >$INIT/.bashrc
+
+        exec bash --init-file $INIT/.bashrc
+      '';
+      zshHook = ''
+        ZDOTDIR_=$ZDOTDIR
+        if [[ -z "$ZDOTDIR_" ]]; then
+          ZDOTDIR_=$HOME
+        fi
+
+        export ZDOTDIR=/tmp/bigShellNix
+        mkdir -p $ZDOTDIR
+
+        printf "%s\n" \
+          "export ZDOTDIR=$ZDOTDIR_" \
+          'source $ZDOTDIR/.zshrc' \
+          '${shellHook}' \
+        >$ZDOTDIR/.zshrc
+
         exec zsh
       '';
     }
-    .${shellName}
+    ."${shellName}Hook"
   );
   override = {
     buildInputs = buildInputs ++ [ pkgs.${shellName} ];
